2ec44da6d4bce35523bcc83148f4c7fc8a282aa1
RuslanKLESK <rus.lan@mail.ru>
2021-10-18 18:29:45 +0300

    lesson4 Seed, User#method, Test.method

---
+++ db/migrate/20211018121828_create_table_tests_users.rb
@@ -0,0 +1,8 @@
+class CreateTableTestsUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table "tests_users" do |t|
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_18_122844) do
+ActiveRecord::Schema.define(version: 2021_10_18_121828) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,7 +44,14 @@ ActiveRecord::Schema.define(version: 2021_09_18_122844) do
     t.bigint "category_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
     t.index ["category_id"], name: "index_tests_on_category_id"
+    t.index ["user_id"], name: "index_tests_on_user_id"
+  end
+
+  create_table "tests_users", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -58,4 +65,5 @@ ActiveRecord::Schema.define(version: 2021_09_18_122844) do
   add_foreign_key "answers", "questions"
   add_foreign_key "questions", "tests"
   add_foreign_key "tests", "categories"
+  add_foreign_key "tests", "users"
 end
